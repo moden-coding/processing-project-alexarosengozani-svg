517d8e80440340fab7b726de974e07a153d6305a
alexarosengozani-svg <alexarosengozani@gmail.com>
2025-10-29 13:50:57 -0400

    working

--- src/App.java
+++ src/App.java
@@ -12,7 +12,7 @@ public class App extends PApplet{
     int ballG = 0;
     int ballB = 0;
 
-    int scene = 2; //sets the the beggining scene that starts everything off
+    int scene = 1; //sets the the beggining scene that starts everything off
     int number = 3;
 
     PImage heart; //inserts the heart image
@@ -67,66 +67,52 @@ public class App extends PApplet{
         text("Welcome to Pong!", 116,200); //puts in my text
         textSize(33); //sets the size of my text
         fill(0); // //makes my text white
-        text("press the the box below to start your game",45,300); //puts in my text
-
-        fill(80,30,200); //gives my box color
-        rect(230,350,198,80); //add in box for continue button
-        textSize(35);
-        fill(0);
-        text("START GAME",236,400);
+        text("press the the box below to view the rules",55,300); //puts in my text
     }
 
     if (scene == 2) { //my countdown. makes my game start
-    background(0);
-    textSize(300);
-    text(number, 240,330);
+    background(230);
+    textSize(100);
+    text("The right paddle is Player1. Player1 use arrow keys.", 100,100);
+    text("The left paddle is Player2. Player2 use wasd.", 200,200);
 
-    delay(1000);  //chatgpt
-    number--; //chatgpt
-
-    if (number < 0) {
-    background(0);
-    textSize(300);
-    text("GO!", 85,350);
-     //chatgpt
-
-    delay(1000);
-
-
-    scene = 3;
+    fill(80,30,200); //gives my box color
+    rect(230,350,198,80); //add in box for continue button
+    textSize(35);
+    fill(0);
+    text("START GAME",236,400);
     }
 
-}
 
     if (scene == 3) {
-        background(230);
-        ballX += ballXSpeed;
-        ballY += ballYSpeed;
+        background(230); //makes the new background grey
+        ballX += ballXSpeed; //makes the ball move side to side
+        ballY += ballYSpeed; //makes the ball move up and down
 
-        if (ballY <= 0 || ballY >= height) {
-        ballYSpeed *= -1;
+        if (ballY <= 0 || ballY >= height) { //when my ball hits the top or bottom, it makes my ball bounce off
+        ballYSpeed *= -1; 
         }
 
-        if (ballX + 20 >= width - 25 && ballY >= paddleA && ballY <= paddleA + 125) {   
-            println("touched right");
+        if (ballX + 20 >= width - 25 && ballY >= paddleA && ballY <= paddleA + 125) { //makes the ball bounce of the right paddle
+            println("touched right"); //helps me test my game and make sure evrything is runnning well
             ballXSpeed *= -1;
             
-            ballR = (int)random(255);
+            ballR = (int)random(255); //makes the ball change color randomly when touching the paddle
             ballG = (int)random(255);
             ballB = (int)random(255); 
         }
 
-        if (ballX - 20 <= 25 && ballY >= paddleB && ballY <= paddleB + 125) {
-            println("touched left");
+        if (ballX - 20 <= 25 && ballY >= paddleB && ballY <= paddleB + 125) { //makes the ball bounce of the left paddle
+            println("touched left"); //helps me test my game and make sure evrything is runnning well
             ballXSpeed *= -1;
 
-            ballR = (int)random(255);
+            ballR = (int)random(255); //makes the ball change color randomly when touching the paddle
             ballG = (int)random(255);
             ballB = (int)random(255);
         }
 
 
-        if(up == true) {
+        if(up == true) { 
             paddleA -= 5;  
         }else if(down == true) {
             paddleA += 5;  
@@ -149,17 +135,17 @@ public class App extends PApplet{
         paddleB = height - 125;
         }
 
-        fill(ballR, ballG, ballB);        
-        ellipse(ballX, ballY, 40, 40);
-        noStroke();
+        fill(ballR, ballG, ballB); //makes it so that the ball can change colors     
+        ellipse(ballX, ballY, 40, 40); //sets the ball size and coordinates
+        noStroke(); //makes there be no border
         
         fill(0); //paddleA
-        rect(width - 25, paddleA, 25, 125);
-        noStroke();
+        rect(width - 25, paddleA, 25, 125); //draws paddleA
+        noStroke(); //makes there be no border
         
         fill(0); //paddleB
-        rect(width - 650, paddleB, 25, 125);
-        noStroke();
+        rect(width - 650, paddleB, 25, 125); //draws paddleB
+        noStroke(); //makes there be no border
         image(heart, 100,100,100,100);
 
         if (ballX >= 650) {
@@ -224,7 +210,7 @@ public class App extends PApplet{
          if (scene == 1) {
             if (mouseX > 230 && mouseX < 428 && mouseY > 350 && mouseY < 430) {
                 System.out.println("yep");
-                scene = 3;
+                scene = 2;
             }
         }
     }
